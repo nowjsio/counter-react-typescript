ffe5a7e5a19e27a6d5b913b7e42e589e4a78be4a
nowjsio <nowjsio@gmail.com>
2023-01-24 21:29:47 +0900

    feat: use 'createAsyncThunk' for asyncIncrementAmount

--- src/features/counter/counterSlice.ts
+++ src/features/counter/counterSlice.ts
@@ -1,20 +1,24 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable no-debugger */
-import { createSlice } from '@reduxjs/toolkit';
-/* eslint-disable no-plusplus */
-/* eslint-disable no-param-reassign */
-// eslint-disable-next-line import/no-cycle
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { fetchCount, FetchCounterReponse } from '../../api/api';
 
 interface CounterState {
   value: number;
+  status: string;
   MIN_VALUE: number;
   MAX_VALUE: number;
 }
+
 // interface IncrementType {
 //   payload: string;
 //   type: 'counter/incrementByAmount';
 // }
-const initialState = { value: 10, MIN_VALUE: 0, MAX_VALUE: 100 } as CounterState;
+const initialState = { value: 10, status: 'init', MIN_VALUE: 0, MAX_VALUE: 100 } as CounterState;
+
+export const incrementAsync = createAsyncThunk('counter/incrementAsync', async (amount: number, _thukAPI) => {
+  const response: FetchCounterReponse = await fetchCount(amount);
+  return response.value;
+});
+
 export const counterSlice = createSlice({
   name: 'counter',
   initialState,
@@ -51,34 +55,67 @@ export const counterSlice = createSlice({
         state.value = result;
       }
     },
+    decrementAsync(state, { payload }) {
+      state.status = payload;
+    },
+    decrementAsyncSuccess(state, { payload }) {
+      state.status = 'success';
+      const result = state.value - payload;
+      if (result < initialState.MIN_VALUE) {
+        state.value = initialState.MIN_VALUE;
+      } else {
+        state.value = result;
+      }
+    },
+    decrementAsyncFailed(state, _action) {
+      state.status = 'failed';
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(incrementAsync.pending, (state, _action) => {
+      state.status = 'pending';
+    });
+    builder.addCase(incrementAsync.fulfilled, (state, action) => {
+      state.status = 'fulfilled';
+      state.value += action.payload;
+    });
+    builder.addCase(incrementAsync.rejected, (state, _action) => {
+      state.status = 'rejected';
+    });
   },
 });
 
-export const { increment, decrement, incrementByAmount, decrementByAmount } = counterSlice.actions;
+export const {
+  increment,
+  decrement,
+  incrementByAmount,
+  decrementByAmount,
+  decrementAsync,
+  decrementAsyncSuccess,
+  decrementAsyncFailed,
+} = counterSlice.actions;
 export const counterActions = counterSlice.actions;
 
 // The function below is called a thunk and allows us to perform async logic. It
 // can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
 // will call the thunk with the `dispatch` function as the first argument. Async
 // code can then be executed and other actions can be dispatched
-export const incrementAsync = (amount: number) => (dispatch: any) => {
-  setTimeout(() => {
-    dispatch(incrementByAmount(amount));
-  }, 1000);
-};
+// export const incrementAsync =
+//   (amount: number) => (dispatch: (arg0: { payload: any; type: 'counter/incrementByAmount' }) => void) => {
+//     setTimeout(() => {
+//       dispatch(incrementByAmount(amount));
+//     }, 1000);
+//   };
 
-export const decrementAsync = (amount: number) => (dispatch: any) => {
-  setTimeout(() => {
-    dispatch(decrementByAmount(amount));
-  }, 1000);
-};
+// export const decrementAsync =
+//   (amount: number) => (dispatch: (arg0: { payload: any; type: 'counter/decrementByAmount' }) => void) => {
+//     setTimeout(() => {
+//       dispatch(decrementByAmount(amount));
+//     }, 1000);
+//   };
 
 export const testFunc = (amount: number) => (dispatch: any) => {
   dispatch(incrementByAmount(amount));
 };
-// The function below is called a selector and allows us to select a value from
-// the state. Selectors can also be defined inline where they're used instead of
-// in the slice file. For example: `useSelector((state) => state.counter.value)`
-// export const selectCount = (state: { counter: { value: number } }) => state.counter.value;
 export const { MAX_VALUE, MIN_VALUE } = initialState;
 export default counterSlice.reducer;
